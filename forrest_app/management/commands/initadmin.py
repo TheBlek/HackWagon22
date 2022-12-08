6c2769b722db8e5abbe09e7022aa3eef1f354b7b
Blek <egorka99966@gmail.com>
2022-12-08 22:39:58 +0700

    cleanupv2

--- forrest_app/management/commands/initadmin.py
+++ forrest_app/management/commands/initadmin.py
@@ -4,9 +4,9 @@ from django.contrib.auth import get_user_model
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        User = get_user_model()
-        if User.objects.count() == 0:
+        user = get_user_model()
+        if user.objects.count() == 0:
             print('Creating admin account')
-            User.objects.create_superuser('admin', 'admin@gmail.com', 'admin').save()
+            user.objects.create_superuser('admin', 'admin@gmail.com', 'admin').save()
         else:
             print('Admin accounts can only be initialized if no Accounts exist')
