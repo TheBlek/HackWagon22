16f751bf00709afdc4b544fb1ef0f4de3be35742
Егор Куклин <40146818+TheBlek@users.noreply.github.com>
2022-12-09 12:00:23 +0700

    Revert "Django layout"

--- forrest_app/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class BonumAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'forrest_app'
-    verbose_name = 'Администрирование бота'
