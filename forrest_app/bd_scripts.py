0fe6b04c23613c12055ad5f916521601ee82bab4
MurenMurenus <murenmurenus@gmail.com>
2022-12-12 14:26:11 +0700

    Audio_processing + to_tokens

--- forrest_app/bd_scripts.py
+++ forrest_app/bd_scripts.py
@@ -1,7 +1,7 @@
 from .models import Items, BotUser
 
 
-def to_bd(tokens, user: BotUser) -> None:
+def to_bd(tokens: list, user: BotUser) -> None:
     '''-пробегаемся по каждому набору [ключ, значение]
            -проверка есть ли ключ в бд
            -если да
@@ -10,7 +10,7 @@ def to_bd(tokens, user: BotUser) -> None:
                -добавляем в бд ключ и значение'''
     values_database = Items.objects.filter(user=user)
     for value_from_voice in tokens:
-        tmp_value: list[Items] = Items.objects.filter(user=user, name=value_from_voice[0])
+        tmp_value = Items.objects.filter(user=user, name=value_from_voice[0])
         if len(tmp_value) > 0:
             tmp_value[0].count += value_from_voice[1]
             tmp_value[0].save()
