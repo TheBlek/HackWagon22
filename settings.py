0902ef366fd18d789735b67343d8a5e1560ee606
Blek <egorka99966@gmail.com>
2022-12-12 14:10:38 +0700

    enabled jazzmin

--- settings.py
+++ settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS: list[str] = []
 # Application definition
 
 INSTALLED_APPS: list[str] = [
-    #'jazzmin',
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
